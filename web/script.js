f20c21983d55d95e7453bc8b9c11e8921a223c6f
Jaareet <jaareet.soporte@gmail.com>
2024-08-16 03:27:34 +0100

    feat(main) Added new files
    - Fully migrated the script to Vue 3 for modern development standards.
    - Refactored and cleaned up all existing code for improved maintainability.
    - Removed all external dependencies to simplify the setup.

--- web/script.js
+++ web/script.js
@@ -1,40 +1,115 @@
-new Vue({
-    el: '#app',
-    data: {},
+const Post = async (url, data) => {
+    try {
+        const response = await axios.post(
+            `https://${GetParentResourceName()}/${url}`,
+            data || {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const app = Vue.createApp({
+    data() {
+        return {
+            show: false,
+            logo: "",
+            audios: {
+                appendSound: new Audio(
+                    "https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/gta4.wav"
+                ),
+                click: new Audio(
+                    "https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/click.wav"
+                ),
+                hover: new Audio(
+                    "https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/hover.wav"
+                ),
+            },
+            buttons: {
+                continue: {
+                    text: "Continue",
+                    icon: "fas fa-play",
+                    action: "exit",
+                },
+                radar: { text: "Map", icon: "fas fa-map", action: "map" },
+                settings: {
+                    text: "Settings",
+                    icon: "fas fa-cog",
+                    action: "settings",
+                },
+                exit: {
+                    text: "Exit",
+                    icon: "fas fa-power-off",
+                    action: "quit",
+                },
+            },
+        };
+    },
     methods: {
-      hideElements() {$('.back').hide()},
-      showBody() {$('.back').fadeIn(0).addClass('fadeIn')},
-      hideBody() {$('.back').addClass('fadeOut'); setTimeout(() => { $('.back').hide().removeClass('fadeOut'); }, 500)},
-      sendAction(action) { $.post('https://cT-SimplePauseMenu/SendAction', JSON.stringify({ action })) },
+        getDelayFromKey(key) {
+            if (!this.buttons || !key) return 0;
+            return Object.keys(this.buttons).indexOf(key) * 0.1;
+        },
+        async sendAction(action) {
+            this.show = false;
+            if (action !== "exit") this.sendAction("exit");
+            if (action === "quit") {
+                window.invokeNative("quit", "you left the game");
+            } else {
+                try {
+                    await Post("SendAction", { action });
+                } catch (error) {}
+            }
+        },
+        playSound(audio) {
+            audio.currentTime = 0;
+            audio.play();
+        },
+        playHover() {
+            this.playSound(this.audios.hover);
+        },
+        playClick() {
+            this.playSound(this.audios.click);
+        },
+        playOpen() {
+            this.playSound(this.audios.appendSound);
+        },
+        setup({ translatedTbl, serverIcon }) {
+            Object.keys(this.buttons).forEach((key) => {
+                this.buttons[key].text = translatedTbl[key];
+            });
+            this.logo = serverIcon;
+        },
+        openEsc() {
+            this.show = true;
+            this.playOpen();
+        },
+        closeEsc() {
+            this.show = false;
+            this.sendAction("exit");
+        },
     },
     mounted() {
-      var appendSound = new Audio('https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/gta4.wav');
-      var click = new Audio('https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/click.wav');
-      var hover = new Audio('https://r2.fivemanage.com/QbMW9SvJrh8gWirlVt5Ee/hover.wav');
+        this.show = !navigator.userAgent.includes("CitizenFX");
 
-      appendSound.volume = 0.35;
-      click.volume = 0.75;
-      hover.volume = 0.75;
+        this.audios.appendSound.volume = 0.35;
+        this.audios.click.volume = 0.75;
+        this.audios.hover.volume = 0.75;
 
-      this.hideElements();
+        window.addEventListener("message", ({ data }) => {
+            if (this[data.action]) this[data.action](data);
+        });
 
-      window.addEventListener('message', (event) => {
-        const {action} = event.data;
-        if (action === 'openEsc') {
-          this.showBody();
-          appendSound.currentTime = 0; 
-          appendSound.play();
-        } else if (action === 'closeEsc') {
-          this.hideBody();
-        }
-      });
-
-      $(document).on('click', '#continue', () => { this.sendAction('exit') });
-      $(document).on('click', '#radar', () => { this.sendAction('map'); this.sendAction('exit') });
-      $(document).on('click', '#settings', () => { this.sendAction('settings'); this.sendAction('exit') });
-      $(document).on('click', '#exit', () => { window.invokeNative('quit', 'you left the game'); });
-      $(document).on('click', '#continue, #radar, #settings, #exit', function() {click.currentTime = 0; click.play()});
-      $(document).on('mouseenter', '#continue, #radar, #settings, #exit', function() {hover.currentTime = 0; hover.play()});
-      document.onkeydown = ({ key }) => key === 'Escape' && this.sendAction('exit')
+        document.onkeydown = ({ key }) => {
+            if (key === "Escape") this.closeEsc();
+        };
     },
 });
+
+app.mount("#app");
